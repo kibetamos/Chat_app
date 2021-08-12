6b61337b219f111459d0b44822c7ed82aa10663a
kibet kilel <amos.rotich@strathmore.edu>
2021-08-12 12:35:41 +0300

    add room functionality

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
-from chat.models import Room, Message
+from .models import Room, Message
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def room(request, room):
+    return render(request, 'room.html')
+
+def checkview(request):
+    room = request.POST['room_name'],
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
