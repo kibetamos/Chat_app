4d125d3fafb4fc82e5f66ed75fa3f97ea2c82f2b
kibetamos <amos.rotich@strathmore.edu>
2025-07-03 10:41:08 +0300

    update on the libraries and settings

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +132,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
